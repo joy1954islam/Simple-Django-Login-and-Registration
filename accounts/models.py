8298738841d1d786e1a270ca08d6cd9b9176ae65
joyislam1954 <joyislam1954@gmail.com>
2020-06-19 07:04:08 +0600

    simple login and registration system

---
+++ accounts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Activation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    code = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(blank=True)
